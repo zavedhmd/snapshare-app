65d41423da660b21b77b2e603ebc7714511e00f2
Zaved Ahmed <zavedhmd@gmail.com>
2020-07-17 20:26:14 +0530

    Initial commit of snapshare

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './styles/styles.scss';
+import 'normalize.css/normalize.css'
+
+ReactDOM.render(<p>This is a boiler plate</p>, document.getElementById('app'))
